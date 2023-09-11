b9a861cf6463064fc35aecf4c573c1371fd70d02
EliseoSmutt <eliseosmutt@outlook.com>
2023-09-10 22:55:56 -0300

    arreglo de clase

--- Form1.cs
+++ Form1.cs
@@ -6,22 +6,21 @@ namespace EliseoSmuttEjer5
         {
             InitializeComponent();
         }
-        private void RegistrarDatos(string nombre, string mail, string texto)
-        {
-            DateTime fecha = DateTime.Now;
-            string datos = $"Nombre: {nombre}\r\n Mail {mail}\r\n Su mensaje: {texto}\r\n Fecha de envio del mensaje: {fecha}";
-            lbl1.Text = datos;
-        }
-        private void btn_Click(object sender, EventArgs e)
+        private void RegistrarDatos()
         {
             string nombre = tb1.Text;
             string mail = tb2.Text;
             string texto = tb3.Text;
-
-            RegistrarDatos(nombre, mail, texto);
+            DateTime fecha = DateTime.Now;
+            string datos = $"Nombre: {nombre}\r\n Mail: {mail}\r\n Su mensaje: {texto}\r\n Fecha de envio del mensaje: {fecha}";
+            lbl1.Text = datos;
             tb1.Clear();
             tb2.Clear();
             tb3.Clear();
+        }
+        private void btn_Click(object sender, EventArgs e)
+        {
+            RegistrarDatos();
         }
     }
 }
